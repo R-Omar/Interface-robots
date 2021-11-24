23276acb75393ac35a83c8bc3bf3995b50a6d742
Omar RIAHI <76665705+R-Omar@users.noreply.github.com>
2021-11-24 13:41:44 +0100

    Adding main card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainCardRobotComponent } from './main-card-robot/main-card-robot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCardRobotComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-card-robot/main-card-robot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-card-robot',
+  templateUrl: './main-card-robot.component.html',
+  styleUrls: ['./main-card-robot.component.css']
+})
+export class MainCardRobotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
